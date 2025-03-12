0c362838520b3c3258f4300627d2ee80986ba406
Jaqueline Sobral <jaqueline.sobral@zup.com.br>
2025-03-12 14:35:40 -0300

    chore: inicializa projeto spring initializr com dependências spring security, data, validation, web, postgres e lombok

---
+++ src/test/java/com/zup/gerenciarImpostos/GerenciarImpostosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.gerenciarImpostos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarImpostosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
