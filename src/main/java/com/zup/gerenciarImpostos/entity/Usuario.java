47ef029a011231901fc15e695231a4e7b7128db8
Jaqueline Sobral <jaqueline.sobral@zup.com.br>
2025-03-12 15:47:21 -0300

    feat: Adiciona classe usuario com atributos no pacote entities

---
+++ src/main/java/com/zup/gerenciarImpostos/entity/Usuario.java
@@ -0,0 +1,34 @@
+package com.zup.gerenciarImpostos.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@NoArgsConstructor
+@Data
+@Table(name = "usuarios")
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nomeUsuario;
+
+    @Column(nullable = false, unique = true)
+    private String senha;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuarios_roles",
+            joinColumns = @JoinColumn(name = "usuarios_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id", referencedColumnName = "id")
+    )
+    private Set<Roles> roles;
+
+    private void Usuario(String nomeUsuario){
+    }
+}
