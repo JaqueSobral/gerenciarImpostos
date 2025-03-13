764fcc4d03ce6a41247d88e6cbfb3deb37ef44e1
Jaqueline Sobral <jaqueline.sobral@zup.com.br>
2025-03-13 10:32:19 -0300

    feat: Adiciona as novas classes na camada repositories(impostoRepository, RoleRepository, UsuarioRepository)

---
+++ src/main/java/com/zup/gerenciarImpostos/repositories/ImpostoRepository.java
@@ -0,0 +1,4 @@
+package com.zup.gerenciarImpostos.repositories;
+
+public interface ImpostoRepository {
+}
---
+++ src/main/java/com/zup/gerenciarImpostos/repositories/RoleRepository.java
@@ -0,0 +1,6 @@
+package com.zup.gerenciarImpostos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository {
+}
