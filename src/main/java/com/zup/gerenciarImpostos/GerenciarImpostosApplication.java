fb9047ce4ed74030439789069cecae7f40e8b79c
Jaqueline Sobral <jaqueline.sobral@zup.com.br>
2025-03-24 17:25:22 -0300

    Feat: adiciona a classe de application do projeto

--- src/main/java/com/zup/gerenciarImpostos/GerenciarImpostosApplication.java
+++ src/main/java/com/zup/gerenciarImpostos/GerenciarImpostosApplication.java
@@ -2,12 +2,10 @@ package com.zup.gerenciarImpostos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class GerenciarImpostosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciarImpostosApplication.class, args);
 	}
-
 }
